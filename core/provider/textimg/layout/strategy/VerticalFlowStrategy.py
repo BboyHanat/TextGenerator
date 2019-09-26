d01e02715fefe00b96a0582c0a4393fe9669cb52
lijianan <lijianan@tezign.com>
2019-09-26 15:42:04 +0800

    add: 竖直流式布局策略

---
+++ core/provider/textimg/layout/strategy/VerticalFlowStrategy.py
@@ -0,0 +1,36 @@
+from core.provider.textimg.layout.strategy import Strategy
+
+
+class VerticalFlowStrategy(Strategy):
+    """
+    生成一个竖直流式排布的文本贴图布局
+    """
+
+    def logic(self, block_group, next_block) -> bool:
+        init_x = block_group.group_box[0]
+        init_y = block_group.group_box[1]
+        next_x = init_x
+        next_y = init_y
+
+        max_r = 0
+        lasb_block = None
+
+        for block in block_group.block_list:
+            r = block.outer_box[2]
+            if max_r < r:
+                max_r = r
+
+            lasb_block = block
+
+        if lasb_block:
+            next_x = lasb_block.outer_box[0]
+            next_y = lasb_block.outer_box[3] + 1
+
+        next_block.locate_by_outter(next_x, next_y)
+        if self.check_is_out(block_group=block_group, block=next_block):
+            next_x = max_r + 1
+            next_y = init_y
+            next_block.locate_by_outter(next_x, next_y)
+            if self.check_is_out(block_group=block_group, block=next_block):
+                return False
+        return True
--- core/provider/textimg/layout/strategy/strategy_controller.py
+++ core/provider/textimg/layout/strategy/strategy_controller.py
@@ -3,12 +3,19 @@ from utils.random_tools import Random
 from core.provider.textimg.layout.strategy.HorizontalStrategy import HorizontalStrategy
 from core.provider.textimg.layout.strategy.VerticalStrategy import VerticalStrategy
 from core.provider.textimg.layout.strategy.HorizontalFlowStrategy import HorizontalFlowStrategy
+from core.provider.textimg.layout.strategy.VerticalFlowStrategy import VerticalFlowStrategy
 
 horizontal_strategy = HorizontalStrategy()
 vertical_strategy = VerticalStrategy()
 horizontal_flow_strategy = HorizontalFlowStrategy()
+vertical_flow_strategy = VerticalFlowStrategy()
 
-strategy_list = [horizontal_strategy, vertical_strategy, horizontal_flow_strategy]
+strategy_list = [
+    horizontal_strategy,
+    vertical_strategy,
+    horizontal_flow_strategy,
+    vertical_flow_strategy
+]
 
 
 def pick(target: Strategy = None) -> Strategy:
